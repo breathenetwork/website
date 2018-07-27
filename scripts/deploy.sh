bbd029c363b096e0b714ae7285476dc00137c1a3
Alexander Tumin <iamtakingiteasy@eientei.org>
2018-07-27 22:45:41 +0300

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-echo succ
+openssl aes-256-cbc -K $encrypted_ddf7872b8477_key -iv $encrypted_ddf7872b8477_iv -in secret.key.enc -out secret.key -d
+ssh -i secret.key -p50265 travis@breathe.network 'rm -r /home/http/breathe/website/build'
+scp -i secret.key -P50265 -r build travis@breathe.network:/home/http/breathe/website
