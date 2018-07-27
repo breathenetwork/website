eb3231eda5dea260e43c48e968e1c7847a3f7034
Alexander Tumin <iamtakingiteasy@eientei.org>
2018-07-27 23:31:46 +0300

    deploy restart

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,4 @@ openssl aes-256-cbc -K $encrypted_ddf7872b8477_key -iv $encrypted_ddf7872b8477_i
 chmod 0600 scripts/secret.key
 ssh -i scripts/secret.key -p50265 travis@breathe.network 'rm -rf /home/http/breathe/website/dist'
 scp -i scripts/secret.key -P50265 -r dist travis@breathe.network:/home/http/breathe/website
+ssh -i scripts/secret.key -p50265 travis@breathe.network 'sudo systemctl restart breathenetwork.service'
