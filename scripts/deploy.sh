ae56d2ef59dbdcc5f805b302861325210343a334
Alexander Tumin <iamtakingiteasy@eientei.org>
2018-07-27 22:53:23 +0300

    paths

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-openssl aes-256-cbc -K $encrypted_ddf7872b8477_key -iv $encrypted_ddf7872b8477_iv -in secret.key.enc -out secret.key -d
-ssh -i secret.key -p50265 travis@breathe.network 'rm -r /home/http/breathe/website/build'
-scp -i secret.key -P50265 -r build travis@breathe.network:/home/http/breathe/website
+openssl aes-256-cbc -K $encrypted_ddf7872b8477_key -iv $encrypted_ddf7872b8477_iv -in scripts/secret.key.enc -out scripts/secret.key -d
+ssh -i scripts/secret.key -p50265 travis@breathe.network 'rm -r /home/http/breathe/website/build'
+scp -i scripts/secret.key -P50265 -r build travis@breathe.network:/home/http/breathe/website
